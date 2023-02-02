d8073a839d0c7ea552c8032f3b76970cc61c9777
JeremyWesthead <jeremywesthead3@gmail.com>
2023-02-02 10:24:06 +0000

    Rename policy fetching python to be more explicit to its function

--- policy-delete.sh
+++ policy-delete.sh
@@ -6,7 +6,7 @@
 #This fetches the OCIDs of the policies with the 5 letter code of the stack, then deletes
 #Usage: bash policy-delete.sh <sandbox OCID> <5 letter stack code>
 
-for policy in $(oci iam policy list -c $1 --all | python get.py $2);
+for policy in $(oci iam policy list -c $1 --all | python get-policy-ids.py $2);
 do
     echo Deleting $policy
     oci iam policy delete --policy-id $policy --wait-for-state SUCCEEDED  --wait-for-state FAILED
